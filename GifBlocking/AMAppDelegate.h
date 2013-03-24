27b4a12727a6da8c2935eae1f1edda8c3208e578
A M Leszkiewicz <amleszk@gmail.com>
2013-03-25 00:08:17 +1100

    Initial Commit

---
+++ GifBlocking/AMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AMAppDelegate.h
+//  GifBlocking
+//
+//  Created by al on 25/03/13.
+//  Copyright (c) 2013 amleszk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
